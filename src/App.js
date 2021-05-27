c1b647783144246fd4554201425fdaa9699c305a
Sarthak <sarthaksnh5@gmail.com>
2021-05-27 10:03:42 +0530

    ProjectSetup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+    <h3>PortFolio Website ..</h3>
+    </>
+  );
+}
+
+export default App;
